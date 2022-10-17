a08daf77f389a245ec3270cf4165e7a31927c7f7
Mauro <maurogutierreztormo@gmail.com>
2022-10-17 09:59:34 +0200

    añadi mi nombre completo

--- HolaAlumnos.java
+++ HolaAlumnos.java
@@ -2,6 +2,8 @@ public class HolaAlumnos {
  
 	public static void main(String[] args) {		
 		System.out.println("Hola alumnos de DAM1!!");
+		System.out.println("Mauro Gutierrez Tormo");
+
 	}
  
 }
